fec7686ea15b021757703f2fdad6bb18dbff422d
takeokunn <bararararatty@gmail.com>
2026-08-02 21:08:12 +0900

    chore(release): v1.3.0

--- demo/main.lisp
+++ demo/main.lisp
@@ -85,7 +85,7 @@ only ever calls PARSE-ARGV on a flat argv."
                        :description "List remotes."
                        :handler #'%remote-list))))
 
-(defparameter +demo-version+ "1.2.0"
+(defparameter +demo-version+ "1.3.0"
   "The version `cl-cli-demo --version` prints.
 
 A literal, not `(asdf:component-version (asdf:find-system \"cl-cli/demo\"))`:
